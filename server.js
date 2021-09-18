1a82dbbab831b39e3af14882bca6ac202fccd5f7
aga87 <aga.labonarska@outlook.com>
2021-09-18 13:47:24 +0200

    Deployment - serve static assets in production

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const plants = require('./routes/api/plants');
 
@@ -19,6 +20,14 @@ mongoose
 
 // Routes
 app.use('/api/plants', plants);
+
+// Deployment: serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000; // Deploy to Heroku or 5000
 
