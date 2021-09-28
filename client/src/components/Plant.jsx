2c2d30bfa6ea07fcebfdfade48f7feeeb13086f6
aga87 <aga.labonarska@outlook.com>
2021-09-28 12:39:09 +0200

    Plant card - display sowing/harvesting data in tabular calendar view

--- client/src/components/Plant.jsx
+++ client/src/components/Plant.jsx
@@ -1,7 +1,9 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import DeletePlantBtn from './DeletePlantBtn';
 import { months, plantCategories } from '../utils/constants';
+import MonthHeadings from './MonthHeadings';
+import MonthData from './MonthData';
+import DeletePlantBtn from './DeletePlantBtn';
 
 const Plant = props => {
   const {
@@ -20,11 +22,22 @@ const Plant = props => {
       <h1>{name}</h1>
       <p>{desc}</p>
       <p>{category || 'Uncategorised'}</p>
+      <table>
+        <thead>
+          <tr>
+            <MonthHeadings />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <MonthData monthStart={sowFrom} monthEnd={sowUntil} />
+          </tr>
+          <tr>
+            <MonthData monthStart={harvestFrom} monthEnd={harvestUntil} />
+          </tr>
+        </tbody>
+      </table>
       <DeletePlantBtn id={id} />
-      <p>Sow from: {sowFrom}</p>
-      <p>Sow until: {sowUntil}</p>
-      <p>Harvest from: {harvestFrom}</p>
-      <p>Harvest until: {harvestUntil}</p>
     </figure>
   );
 };
