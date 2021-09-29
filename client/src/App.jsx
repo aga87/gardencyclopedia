2e9e847d948cc0363c57b49d15a80c6d66830dfb
aga87 <aga.labonarska@outlook.com>
2021-09-29 12:44:13 +0200

    Convert Loader into a reusable Spinner component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import './css/index.css';
 import { useSelector } from 'react-redux';
-import { selectPlantModalIsOpen } from './redux/reducers/index';
-import Loader from './components/Loader';
+import {
+  selectPlantModalIsOpen,
+  selectIsLoading
+} from './redux/reducers/index';
+import Spinner from './components/Spinner';
 import PlantList from './components/PlantList';
 import PlantModal from './components/PlantModal';
 import Nav from './components/Nav';
 
 const App = () => {
   const plantModalIsOpen = useSelector(selectPlantModalIsOpen);
+  const plantsAreLoading = useSelector(selectIsLoading);
 
   return (
     <div className="App">
       {plantModalIsOpen && <PlantModal />}
       <Nav />
-      <Loader />
+      <Spinner isLoading={plantsAreLoading} />
       <PlantList />
     </div>
   );
--- client/src/components/Icon.jsx
+++ client/src/components/Icon.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faPlus, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 const Icon = props => {
   const { name } = props;
 
   if (name === 'menu') return <FontAwesomeIcon icon={faBars} />;
   if (name === 'plus') return <FontAwesomeIcon icon={faPlus} />;
+  if (name === 'spinner') return <FontAwesomeIcon icon={faSpinner} spin />;
   return null;
 };
 
 Icon.propTypes = {
-  name: PropTypes.oneOf(['menu', 'plus']).isRequired
+  name: PropTypes.oneOf(['menu', 'plus', 'spinner']).isRequired
 };
 
 export default Icon;
