c3fb52fc0dda8e0bd39fedbc83b36dd15c0b06d2
aga87 <aga.labonarska@outlook.com>
2021-10-11 20:38:49 +0200

    Prettify common types

--- client/src/utils/common-types.ts
+++ client/src/utils/common-types.ts
@@ -1,13 +1,21 @@
 import { months } from './constants';
 
 // arrays in readonly mode
-const plantCategories= ['', 'uncategorised',
-'vegetables',
-'fruits',
-'herbs',
-'flowers'] as const;
+const plantCategories = [
+  '',
+  'uncategorised',
+  'vegetables',
+  'fruits',
+  'herbs',
+  'flowers'
+] as const;
 const monthsArr = ['', ...months] as const;
-const sortOptions = ['name', 'category', 'sowing time', 'harvest time'] as const;
+const sortOptions = [
+  'name',
+  'category',
+  'sowing time',
+  'harvest time'
+] as const;
 
 export type Month = typeof monthsArr[number];
 export type Category = typeof plantCategories[number];
