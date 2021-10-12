4057745ff482eb543919a63d7244f0406f60b053
aga87 <aga.labonarska@outlook.com>
2021-10-12 18:58:50 +0200

    Extract PlantMenu as a component

--- client/src/components/PlantEntry.tsx
+++ client/src/components/PlantEntry.tsx
@@ -1,57 +1,20 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { deletePlant } from '../redux/actions/plantsActions';
-import { openPlantModal } from '../redux/actions/uiActions';
+import React from 'react';
 import CalendarChart from './CalendarChart';
-import Btn from './Btn';
-import Icon from './Icon';
+import PlantMenu from './PlantMenu';
 
 type PlantEntryProps = {
   plant: Plant;
 };
 
 const PlantEntry = ({ plant }: PlantEntryProps): JSX.Element => {
-  const [isOpen, setIsOpen] = useState(false);
-  const dispatch = useDispatch();
   const { name, desc, category, sowFrom, sowUntil, harvestFrom, harvestUntil } =
     plant;
-  const id = plant._id;
-
-  const handleDeleteClick = () => {
-    dispatch(deletePlant(id as string));
-  };
-
-  const handleEditClick = () => {
-    dispatch(openPlantModal(plant));
-  };
-
-  const handleMoreClick = () => {
-    setIsOpen(!isOpen);
-  };
 
   return (
     <div>
       <header>
         <h2>{name}</h2>
-        <Btn icon={<Icon name="more" />} handleClick={handleMoreClick} />
-        {isOpen && (
-          <ul>
-            <li>
-              <Btn
-                icon={<Icon name="trash" />}
-                text="Delete"
-                handleClick={handleDeleteClick}
-              />
-            </li>
-            <li>
-              <Btn
-                icon={<Icon name="edit" />}
-                text="Edit"
-                handleClick={handleEditClick}
-              />
-            </li>
-          </ul>
-        )}
+        <PlantMenu plant={plant} />
       </header>
       <p>{desc}</p>
       <p>{category}</p>
