344c8c52088f6c2175a0dda065a3d03390e094c8
aga87 <aga.labonarska@outlook.com>
2021-10-13 21:00:23 +0200

    POST - Register new user with a hashed password

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 const User = require("../../models/User");
 
 // @route POST api/users
 // @desc Register new user
 // @access Public
 router.post("/", (req, res) => {
-  res.send("Register");
+  const { username, email, password } = req.body;
+  if (!username || !email || !password)
+    return res.status(422).json({ Error: "Please enter all fields." });
+
+  User.findOne({ email }).then((user) => {
+    if (user) return res.status(400).json({ Message: "User already exists." });
+    const newUser = new User({ email, username, password });
+    // Create salt and hash
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
+        newUser.save().then((user) => {
+          res.json({
+            user: {
+              id: user.id,
+              username: user.username,
+              email: user.email
+            }
+          });
+        });
+      });
+    });
+  });
 });
 
 module.exports = router;
