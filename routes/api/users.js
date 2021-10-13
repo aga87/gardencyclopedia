f895b97c1570aec288d8f64a0f62fc47a829e70d
aga87 <aga.labonarska@outlook.com>
2021-10-13 19:50:58 +0200

    Set up a mock POST route for registering users

---
+++ routes/api/users.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../../models/User");
+
+// @route POST api/users
+// @desc Register new user
+// @access Public
+router.post("/", (req, res) => {
+  res.send("Register");
+});
+
+module.exports = router;
