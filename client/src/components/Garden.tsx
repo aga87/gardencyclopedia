a1df1b0fdced339b91cdeebe174d0f92bebc6979
aga87 <aga.labonarska@outlook.com>
2021-10-27 12:45:57 +0200

    Style Garden view

--- client/src/components/Garden.tsx
+++ client/src/components/Garden.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
+import Icon from './Icon';
 
-const Garden = (): JSX.Element => (
-  <section>
-    <h1>Garden (Coming Soon):</h1>
-    <ul>
-      <li>Keep track of all plants in your garden.</li>
-      <li>Record plant location, growing medium and conditions.</li>
-      <li>Record your gardening progress for each plant.</li>
-      <li>Record harvesting time and yields.</li>
-      <li>Archive selected plants for future reference.</li>
-    </ul>
-  </section>
-);
+const Garden = (): JSX.Element => {
+  const upcomingFeatures = [
+    'Keep track of all plants in your garden.',
+    'Record plant location, growing medium and conditions.',
+    'Record your gardening progress for each plant.',
+    'Record harvesting time and yields.',
+    'Archive selected plants for future reference.'
+  ];
+
+  const upcomingFeaturesListItems = upcomingFeatures.map(feature => (
+    <li>
+      <Icon name="seedling" /> {feature}
+    </li>
+  ));
+
+  return (
+    <section className="c-garden">
+      <h1 className="c-garden__title t1">
+        <span className="c-garden__logo">
+          <Icon name="seedling" />
+        </span>
+        <br />
+        Garden
+        <br /> Coming Soon:
+      </h1>
+      <ul className="c-garden__list">{upcomingFeaturesListItems}</ul>
+    </section>
+  );
+};
 
 export default Garden;
