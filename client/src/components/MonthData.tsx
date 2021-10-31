570c070a0c048742e0b2e8bd6c57312bd552b21d
aga87 <aga.labonarska@outlook.com>
2021-10-31 17:45:49 +0100

    Style CalendarChart

--- client/src/components/MonthData.tsx
+++ client/src/components/MonthData.tsx
@@ -2,29 +2,38 @@ import React from 'react';
 import { months } from '../utils/constants';
 
 type MonthDataProps = {
+  variant: 'sow' | 'harvest';
   monthStart: Month;
   monthEnd: Month;
 };
 
-const MonthData = ({ monthStart, monthEnd }: MonthDataProps): JSX.Element => {
+const MonthData = ({
+  variant,
+  monthStart,
+  monthEnd
+}: MonthDataProps): JSX.Element => {
+  const className = `month-data month-data--${variant}`;
+
   const firstIndex = months.indexOf(monthStart);
   const lastIndex = months.indexOf(monthEnd);
   const dataIsMissing = firstIndex === -1 || lastIndex === -1;
 
   const monthRow = months.map((month, i) => {
-    if (dataIsMissing) return <td key={month}>no</td>;
+    if (dataIsMissing) return <td key={month} className="month-data" />;
     if (firstIndex < lastIndex) {
-      if (i >= firstIndex && i <= lastIndex) return <td key={month}>yes</td>;
-      return <td key={month}>no</td>;
+      if (i >= firstIndex && i <= lastIndex)
+        return <td key={month} className={className} />;
+      return <td key={month} className="month-data" />;
     }
     if (firstIndex > lastIndex) {
-      if (i <= lastIndex) return <td key={month}>yes</td>;
-      if (i > lastIndex && i < firstIndex) return <td key={month}>no</td>;
-      return <td key={month}>yes</td>;
+      if (i <= lastIndex) return <td key={month} className={className} />;
+      if (i > lastIndex && i < firstIndex)
+        return <td key={month} className="month-data" />;
+      return <td key={month} className={className} />;
     }
     // if equal
-    if (i === firstIndex) return <td key={month}>yes</td>;
-    return <td key={month}>no</td>;
+    if (i === firstIndex) return <td key={month} className={className} />;
+    return <td key={month} className="month-data" />;
   });
 
   return <>{monthRow}</>;
