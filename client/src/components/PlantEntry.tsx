5fd0fb09578d78265d86761f63de5800ff65d256
aga87 <aga.labonarska@outlook.com>
2021-11-02 18:34:14 +0100

    Extract PlantEntryHeader as a component

--- client/src/components/PlantEntry.tsx
+++ client/src/components/PlantEntry.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import { capitalize } from '../utils/text-utils';
+import PlantEntryHeader from './PlantEntryHeader';
 import CalendarChart from './CalendarChart';
-import PlantMenu from './Menus/PlantMenu';
 
 type PlantEntryProps = {
   plant: Plant;
 };
 
 const PlantEntry = ({ plant }: PlantEntryProps): JSX.Element => {
-  const { name, desc, category, sowFrom, sowUntil, harvestFrom, harvestUntil } =
+  const { desc, category, sowFrom, sowUntil, harvestFrom, harvestUntil } =
     plant;
 
   return (
     <div className="c-plant-entry">
-      <header className="c-plant-entry__header">
-        <h2 className="c-plant-entry__title t5">{name}</h2>
-        <div className="c-plant-entry__menu-btn">
-          <PlantMenu plant={plant} />
-        </div>
-      </header>
+      <PlantEntryHeader plant={plant} />
       <p className="c-plant-entry__desc">{desc}</p>
       <p className="c-plant-entry__category s1">{capitalize(category)}</p>
       <CalendarChart
