b676e30efdd0442d8d84375fba882f03c7fd0ab3
aga87 <aga.labonarska@outlook.com>
2021-11-03 21:03:06 +0100

    Rename file

--- routes/api/plants.js
+++ routes/api/plants.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
-const plantCtrl = require('../../controllers/plantCtrl');
+const plantsCtrl = require('../../controllers/plantsCtrl');
 
 // @route GET api/plants
 // @desc Get all plants
 // @access Private
-router.get('/', auth, plantCtrl.getAll);
+router.get('/', auth, plantsCtrl.getAll);
 
 // @route POST api/plants
 // @desc Add a plant
 // @access Private
-router.post('/', auth, plantCtrl.add);
+router.post('/', auth, plantsCtrl.add);
 
 // @route DELETE api/plants/:id
 // @desc Delete a plant
 // @access Private
-router.delete('/:id', auth, plantCtrl.delete);
+router.delete('/:id', auth, plantsCtrl.delete);
 
 // @route PUT api/plants/:id
 // @desc Update a plant
 // @access Private
-router.put('/:id', auth, plantCtrl.edit);
+router.put('/:id', auth, plantsCtrl.edit);
 
 module.exports = router;
