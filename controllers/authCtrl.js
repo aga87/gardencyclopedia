84cf1e1a5da3a2908bec76109876de8a875778a1
aga87 <aga.labonarska@outlook.com>
2021-11-04 12:20:57 +0100

    Extract authenticate usersCtrl, rewrite with async/await

---
+++ controllers/authCtrl.js
@@ -0,0 +1,37 @@
+const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const authCtrl = {
+  authenticate: async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password)
+      return res.status(422).json('Please enter all fields.');
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json('User does not exist.');
+    // Validate password
+    bcrypt.compare(password, user.password).then((isMatch) => {
+      if (!isMatch) return res.status(400).json('Invalid credentials.');
+      jwt.sign(
+        { id: user.id },
+        config.get('jwtSecret'),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+
+          res.json({
+            token,
+            user: {
+              id: user.id,
+              username: user.username,
+              email: user.email
+            }
+          });
+        }
+      );
+    });
+  }
+};
+
+module.exports = authCtrl;
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,47 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const config = require('config');
-const jwt = require('jsonwebtoken');
 const auth = require('../../middleware/auth');
-
 const User = require('../../models/User');
+const authCtrl = require('../../controllers/authCtrl');
 
 // @route POST api/auth
 // @desc Authenticate user
 // @access Public
-router.post('/', (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password)
-    return res.status(422).json('Please enter all fields.');
-
-  User.findOne({ email }).then((user) => {
-    if (!user) return res.status(400).json('User does not exist.');
-
-    // Validate password
-    bcrypt.compare(password, user.password).then((isMatch) => {
-      if (!isMatch)
-        return res.status(400).json('Invalid credentials.');
-      jwt.sign(
-        { id: user.id },
-        config.get('jwtSecret'),
-        { expiresIn: 36000 },
-        (err, token) => {
-          if (err) throw err;
-
-          res.json({
-            token,
-            user: {
-              id: user.id,
-              username: user.username,
-              email: user.email
-            }
-          });
-        }
-      );
-    });
-  });
-});
+router.post('/', authCtrl.authenticate);
 
 // @route GET api/auth/user
 // @desc Get user data to continuously authenticate a logged-in user
