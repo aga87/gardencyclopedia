6a16199af7397255d7fe823d1a9e603fa9f36a78
aga87 <aga.labonarska@outlook.com>
2021-11-08 17:08:02 +0100

    Add stories for Tag nano

--- client/src/components/nano/Tag.tsx
+++ client/src/components/nano/Tag.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { capitalize } from '../utils/text-utils';
+import { capitalize } from '../../utils/text-utils';
 
 type TagProps = {
   tag: string;
 };
 
 const Tag = ({ tag }: TagProps): JSX.Element => (
-  <span className='tag s1'>{capitalize(tag)}</span>
+  <div className='tag s1'>{capitalize(tag)}</div>
 );
 
 export default Tag;
