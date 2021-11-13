4cff48f6ff98c99de6a3a75648ea81813dea926d
aga87 <aga.labonarska@outlook.com>
2021-11-13 15:52:00 +0100

    Fix accidently deleted font link in sb preview-html

--- client/src/components/nano/SubmitButton.stories.tsx
+++ client/src/components/nano/SubmitButton.stories.tsx
@@ -9,30 +9,9 @@ export default {
 
 export const SubmitButtonComponent: ComponentStory<typeof SubmitButton> =
   args => <SubmitButton {...args} />;
+
 SubmitButtonComponent.args = {
   text: 'Submit'
 };
+
 SubmitButtonComponent.storyName = 'SubmitButton';
-
-// export default {
-//   title: 'Nano/SubmitButton',
-//   component: SubmitButton,
-//   argTypes: {
-//     text: {
-//       defaultValue: 'Submit'
-//     }
-//   }
-// } as ComponentMeta<typeof SubmitButton>;
-
-// const Template: ComponentStory<typeof SubmitButton> = args => (
-//   <SubmitButton {...args} />
-// );
-
-// export const Primary = Template.bind({});
-
-// export const Secondary = Template.bind({});
-// Secondary.args = {
-//   variant: 'secondary'
-// };
-
-// FIXME: single story
