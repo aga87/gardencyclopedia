af85abd9328c7e64aced1326e78298414c173d70
aga87 <aga.labonarska@outlook.com>
2021-11-15 17:51:19 +0100

    Add DeleteCofirmationAlert modal component

---
+++ client/src/components/DeleteConfirmationAlert.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import Button from './nano/Button';
+import Icon from './nano/Icon';
+
+type DeleteConfirmationAlertProps = {
+  itemName: string;
+  handleCancel: () => void;
+  handleDelete: () => void;
+};
+
+const DeleteConfirmationAlert = ({
+  itemName,
+  handleCancel,
+  handleDelete
+}: DeleteConfirmationAlertProps): JSX.Element => (
+  <div className='c-alert l-alert'>
+    <div className='c-alert__content l-alert__content'>
+      <header className='c-alert__header'>
+        <h2 className='c-alert__title'>Confirm deletion</h2>
+      </header>
+      <div className='c-alert__body'>
+        <p className='c-alert__msg'>
+          Are you sure you want to delete{' '}
+          <span className='c-alert__item-name'>{itemName}</span> ?<br />
+          The action cannot be undone.
+        </p>
+        <ul className='l-alert__btn-group'>
+          <li className='l-alert__btn-group-item'>
+            <Button
+              variant='secondary'
+              text='Cancel'
+              handleClick={handleCancel}
+            />
+          </li>
+          <li className='l-alert__btn-group-item'>
+            <Button
+              variant='tertiary'
+              icon={<Icon name='trash' />}
+              text='Delete'
+              handleClick={handleDelete}
+            />
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+);
+
+export default DeleteConfirmationAlert;
--- client/src/components/Menus/PlantMenu.tsx
+++ client/src/components/Menus/PlantMenu.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { deletePlant } from '../../redux/actions/plantsActions';
 import { openEditPlantModal } from '../../redux/actions/uiActions';
 import useComponentVisibility from '../../utils/hooks/useComponentVisibility';
+import DeleteConfirmationAlert from '../DeleteConfirmationAlert';
 import IconButton from '../nano/IconButton';
 import Button from '../nano/Button';
 import Icon from '../nano/Icon';
@@ -14,6 +15,7 @@ type PlantMenuProps = {
 const PlantMenu = ({ plant }: PlantMenuProps): JSX.Element => {
   const { ref, isComponentVisible, setIsComponentVisible } =
     useComponentVisibility(false); // Hide menu dropdown on click outside
+  const [isAlertVisible, setIsAlertVisible] = useState(false);
   const dispatch = useDispatch();
 
   const handleMoreClick = () => {
@@ -21,8 +23,17 @@ const PlantMenu = ({ plant }: PlantMenuProps): JSX.Element => {
   };
 
   const handleDeleteClick = () => {
+    setIsAlertVisible(true);
+    setIsComponentVisible(false);
+  };
+
+  const handleConfirmDeleteClick = () => {
     dispatch(deletePlant(plant._id as string));
     setIsComponentVisible(false);
+  };
+
+  const handleCancelDeleteClick = () => {
+    setIsAlertVisible(false);
   };
 
   const handleEditClick = () => {
@@ -31,36 +42,45 @@ const PlantMenu = ({ plant }: PlantMenuProps): JSX.Element => {
   };
 
   return (
-    <nav className='c-plant-menu l-plant-menu'>
-      <IconButton
-        variant='secondary'
-        icon={<Icon name='more' />}
-        ariaLabel={`Open ${plant.name} options`}
-        handleClick={handleMoreClick}
-      />
-      <div ref={ref}>
-        {isComponentVisible && (
-          <ul className='c-plant-menu__dropdown l-plant-menu__dropdown'>
-            <li>
-              <Button
-                variant='block'
-                icon={<Icon name='edit' />}
-                text='Edit'
-                handleClick={handleEditClick}
-              />
-            </li>
-            <li>
-              <Button
-                variant='block'
-                icon={<Icon name='trash' />}
-                text='Delete'
-                handleClick={handleDeleteClick}
-              />
-            </li>
-          </ul>
-        )}
-      </div>
-    </nav>
+    <div>
+      {isAlertVisible && (
+        <DeleteConfirmationAlert
+          itemName={plant.name}
+          handleCancel={handleCancelDeleteClick}
+          handleDelete={handleConfirmDeleteClick}
+        />
+      )}
+      <nav className='c-plant-menu l-plant-menu'>
+        <IconButton
+          variant='secondary'
+          icon={<Icon name='more' />}
+          ariaLabel={`Open ${plant.name} options`}
+          handleClick={handleMoreClick}
+        />
+        <div ref={ref}>
+          {isComponentVisible && (
+            <ul className='c-plant-menu__dropdown l-plant-menu__dropdown'>
+              <li>
+                <Button
+                  variant='block'
+                  icon={<Icon name='edit' />}
+                  text='Edit'
+                  handleClick={handleEditClick}
+                />
+              </li>
+              <li>
+                <Button
+                  variant='block'
+                  icon={<Icon name='trash' />}
+                  text='Delete'
+                  handleClick={handleDeleteClick}
+                />
+              </li>
+            </ul>
+          )}
+        </div>
+      </nav>
+    </div>
   );
 };
 
