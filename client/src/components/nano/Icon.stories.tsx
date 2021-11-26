5e879a0b84aabee4a67722bdae4d9b10291b157e
aga87 <aga.labonarska@outlook.com>
2021-11-26 19:39:56 +0100

    Add Icon stories

--- client/src/components/nano/Icon.stories.tsx
+++ client/src/components/nano/Icon.stories.tsx
@@ -1,13 +1,46 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react';
 import Icon from './Icon';
-import type { IconProps } from './Icon';
+
+type IconProps = React.ComponentProps<typeof Icon>;
 
 export default {
   title: 'Nano/Icon',
   component: Icon
-} as Meta;
+} as Meta<IconProps>;
 
-export const IconComponent: Story<IconProps> = args => <Icon {...args} />;
 
-IconComponent.storyName = 'Icon';
+const iconNames = [
+  'menu',
+  'plus',
+  'spinner',
+  'more',
+  'trash',
+  'edit',
+  'user-cog',
+  'calendar',
+  'seedling',
+  'logout',
+  'close'
+] as IconProps['name'][];
+
+export const All = () => (
+  <div>
+    <h2 style={{ marginBottom: '2em', borderBottom: '2px solid pink' }}>
+      Iconography
+    </h2>
+    {iconNames.map(iconName => (
+      <span style={{ marginRight: '10px' }}>
+        <Icon name={iconName} />
+      </span>
+    ))}
+  </div>
+);
+
+const Template: Story<IconProps> = args => <Icon {...args} />;
+
+export const Example = Template.bind({});
+
+Example.args = {
+  name: 'menu'
+};
