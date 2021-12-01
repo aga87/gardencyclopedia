47a15f030062fd53ae54584fa1f65970ce913e50
aga87 <aga.labonarska@outlook.com>
2021-12-01 12:40:05 +0100

    Add stories for MonthlyDataRow

---
+++ client/src/components/MonthlyDataRow.stories.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+import MonthlyDataRow from './MonthlyDataRow';
+
+type MonthlyDataRowProps = React.ComponentProps<typeof MonthlyDataRow>;
+
+export default {
+  title: 'Components/MonthlyDataRow',
+  component: MonthlyDataRow,
+  decorators: [
+    story => (
+      <table style={{ width: '100%' }}>
+        <tbody>{story()}</tbody>
+      </table>
+    )
+  ]
+} as Meta<MonthlyDataRowProps>;
+
+const Template: Story<MonthlyDataRowProps> = args => (
+  <MonthlyDataRow {...args} />
+);
+
+export const SowingData = Template.bind({});
+
+SowingData.args = {
+  variant: 'sow',
+  monthStart: 'February',
+  monthEnd: 'September'
+};
+
+export const HarvestingData = Template.bind({});
+
+HarvestingData.args = {
+  variant: 'harvest',
+  monthStart: 'May',
+  monthEnd: 'October'
+};
+
+export const NoData = Template.bind({});
+
+NoData.args = {
+  variant: 'harvest',
+  monthStart: '',
+  monthEnd: ''
+};
