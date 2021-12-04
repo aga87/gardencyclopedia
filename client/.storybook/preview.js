c78f64587c22f79ff0699b6e4a5a120e5b6954e0
aga87 <aga.labonarska@outlook.com>
2021-12-04 16:22:07 +0100

    Storybook - add custom viewports

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,24 +1,42 @@
 import '../src/sass/index.scss';
 
+const customViewports = {
+  xss: {
+    name: 'xxs',
+    styles: {
+      width: '320px',
+      height: '100vh'
+    }
+  },
+  xs: {
+    name: 'xs',
+    styles: {
+      width: '600px',
+      height: '100vh'
+    }
+  }
+};
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
+      date: /Date$/
+    }
   },
   backgrounds: {
     default: 'white',
     values: [
       {
         name: 'white',
-        value: '#FFF',
+        value: '#FFF'
       },
       {
         name: 'forest',
-        value: '#1e392a',
-      },
-    ],
-  }
-}
+        value: '#1e392a'
+      }
+    ]
+  },
+  viewport: { viewports: customViewports }
+};
