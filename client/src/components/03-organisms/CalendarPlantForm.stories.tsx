cf19840a36ebec357f228a62f9f57bae6bbb49f1
aga87 <aga.labonarska@outlook.com>
2021-12-05 17:50:08 +0100

    Add stories for CalendarPlantForm

---
+++ client/src/components/03-organisms/CalendarPlantForm.stories.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Story, Meta } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import CalendarPlantForm from './CalendarPlantForm';
+
+const store = {
+  getState: () => ({
+    uiReducer: {
+      plantToEdit: {
+        _id: '',
+        name: '',
+        desc: '',
+        category: '',
+        sowFrom: '',
+        sowUntil: '',
+        harvestFrom: '',
+        harvestUntil: ''
+      }
+    }
+  }),
+  subscribe: () => 0,
+  dispatch: action('dispatch')
+} as any; // FIXME: assign correct type
+
+// Mock Redux store - edit plant form
+const storeEdit = {
+  getState: () => ({
+    uiReducer: {
+      plantToEdit: {
+        _id: '1',
+        name: 'Parsley',
+        desc: 'Curly variety',
+        category: 'herbs',
+        sowFrom: 'March',
+        sowUntil: 'August',
+        harvestFrom: 'May',
+        harvestUntil: 'September'
+      }
+    }
+  }),
+  subscribe: () => 0,
+  dispatch: action('dispatch')
+} as any; // FIXME: assign correct type
+
+export default {
+  title: 'organisms/CalendarPlantForm',
+  component: CalendarPlantForm
+} as Meta;
+
+const Template: Story = args => <CalendarPlantForm />;
+
+export const AddPlantForm = Template.bind({});
+AddPlantForm.decorators = [
+  story => <Provider store={store}>{story()}</Provider>
+];
+
+export const EditPlantForm = Template.bind({});
+EditPlantForm.decorators = [
+  story => <Provider store={storeEdit}>{story()}</Provider>
+];
