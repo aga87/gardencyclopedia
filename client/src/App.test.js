bdb01fcaf28342b449ea9c94f68040cde33beaad
aga87 <aga.labonarska@outlook.com>
2021-12-20 16:59:43 +0100

    Test App

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { cleanup, render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from '../src/redux/store';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+afterEach(cleanup);
+
+test('Renders without crashing with app title', () => {
+  const { getByText } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const appTitle = getByText(/gardencyclopedia/i);
+  expect(appTitle).toBeInTheDocument();
 });
