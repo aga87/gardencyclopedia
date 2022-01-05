50e29551a8d74eda80297c24ae1733a343131d7f
aga87 <aga.labonarska@outlook.com>
2022-01-05 18:09:47 +0100

    Simplify tests for CalendarWithLoading

--- client/src/components/03-organisms/CalendarWithLoading/CalendarWithLoading.test.tsx
+++ client/src/components/03-organisms/CalendarWithLoading/CalendarWithLoading.test.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { render, screen } from '@testing-library/react';
-import { within } from '@testing-library/dom';
+import { byRole } from 'testing-library-selector';
 import userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux';
 import { store } from '../../../redux/store';
@@ -17,6 +17,21 @@ const renderCalendarWithLoading = () =>
     </Provider>
   );
 
+const ui = {
+  calendar: byRole('region', { name: /calendar/i }),
+  listitem: byRole('listitem'),
+  noPlants: byRole('heading', { name: /no plants/i }),
+  plantNav: byRole('navigation', { name: /plant options/i }),
+  openPlantNavBtn: byRole('button', { name: /open options/i }),
+  closePlantNavBtn: byRole('button', { name: /close options/i }),
+  deleteBtn: byRole('button', { name: /delete/i }),
+  editBtn: byRole('button', { name: /edit/i }),
+  plantBtn: byRole('button', { name: /plant/i }),
+  alert: byRole('alertdialog', { name: /confirm deletion/i }),
+  chart: byRole('figure', { name: /chart/i }),
+  statusMsg: byRole('status')
+};
+
 describe('<CalendarWithLoading />', () => {
   test('renders "loading" when getting data from the server', () => {
     renderCalendarWithLoading();
@@ -29,136 +44,96 @@ describe('<CalendarWithLoading />', () => {
       mockedAxios.get.mockResolvedValueOnce({ data: { plants: [] } });
       renderCalendarWithLoading();
       expect(mockedAxios.get).toHaveBeenCalledTimes(1);
-      await screen.findByRole('region', { name: /calendar/i });
-      expect(
-        screen.getByRole('heading', { name: /no plants/i })
-      ).toBeInTheDocument();
+      await ui.calendar.find();
+      expect(ui.noPlants.get()).toBeInTheDocument();
     });
 
-    test('renders fetched data (calendar plant list)', async () => {
+    test('renders fetched data (calendar plant list) with caption', async () => {
       mockedAxios.get.mockResolvedValueOnce({
         data: { plants: mockPlantsData }
       });
       renderCalendarWithLoading();
       expect(mockedAxios.get).toHaveBeenCalledTimes(1);
-      await screen.findByRole('region', { name: /calendar/i });
+      await ui.calendar.find();
       expect(screen.queryByText(/loading/i)).not.toBeInTheDocument();
       // Renders correct number of entries
-      const listItems = screen.getAllByRole('listitem');
+      const listItems = ui.listitem.getAll();
       expect(listItems.length).toEqual(mockPlantsData.length);
       // Correctly renders each plant entry
       mockPlantsData.forEach(plant => {
         expect(
           screen.getByRole('heading', { name: plant.name })
         ).toBeInTheDocument();
-
         if (plant.desc) {
-          expect(screen.getByText(plant.desc)).toBeInTheDocument();
+          expect(
+            screen.getByText(new RegExp(plant.desc, 'i'))
+          ).toBeInTheDocument();
         }
-
         expect(
           screen.getByText(new RegExp(plant.category, 'i'))
         ).toBeInTheDocument();
       });
-      expect(screen.getAllByRole('figure', { name: /chart/i }).length).toBe(
-        mockPlantsData.length
-      );
+      expect(ui.chart.getAll().length).toBe(mockPlantsData.length);
+      // Renders caption
+      const caption = `${mockPlantsData.length} plants`
+      expect(screen.getByText(new RegExp(caption, 'i'))).toBeInTheDocument();
     });
 
     test('renders dropdown menu for each plant entry with "edit", "plant" and "delete" options', async () => {
       mockedAxios.get.mockResolvedValueOnce({
-        data: { plants: [mockPlantsData[0]] }
+        data: { plants: mockPlantsData }
       });
       renderCalendarWithLoading();
       expect(mockedAxios.get).toHaveBeenCalledTimes(1);
-      await screen.findByRole('navigation', { name: /plant options/i });
-      userEvent.click(screen.getByRole('button', { name: /open options/i }));
-      expect(
-        screen.getByRole('button', { name: /delete/i })
-      ).toBeInTheDocument();
-      expect(screen.getByRole('button', { name: /edit/i })).toBeInTheDocument();
-      expect(
-        screen.getByRole('button', { name: /plant/i })
-      ).toBeInTheDocument();
-      expect(
-        screen.queryByRole('button', { name: /open options/i })
-      ).not.toBeInTheDocument();
-      expect(
-        screen.getByRole('button', { name: /close options/i })
-      ).toBeInTheDocument();
+      const plantNavs = await ui.plantNav.findAll();
+      expect(plantNavs.length).toBe(mockPlantsData.length);
+      plantNavs.forEach(plantNav => {
+        userEvent.click(ui.openPlantNavBtn.get(plantNav));
+        expect(ui.deleteBtn.get(plantNav)).toBeInTheDocument();
+        expect(ui.editBtn.get(plantNav)).toBeInTheDocument();
+        expect(ui.plantBtn.get(plantNav)).toBeInTheDocument();
+        expect(ui.openPlantNavBtn.query(plantNav)).not.toBeInTheDocument();
+        expect(ui.closePlantNavBtn.get(plantNav)).toBeInTheDocument();
+      });
     });
 
     describe('when deleting a plant', () => {
       test('choosing delete from the dropdown menu renders deletion confirmation alert', async () => {
         mockedAxios.get.mockResolvedValueOnce({
-          data: { plants: [mockPlantsData[0]] }
+          data: { plants: mockPlantsData }
         });
         renderCalendarWithLoading();
         expect(mockedAxios.get).toHaveBeenCalledTimes(1);
-        await screen.findByRole('navigation', { name: /plant options/i });
-        userEvent.click(screen.getByRole('button', { name: /open options/i }));
-        userEvent.click(screen.getByRole('button', { name: /delete/i }));
-        expect(
-          screen.getByRole('alertdialog', { name: /confirm deletion/i })
-        ).toBeInTheDocument();
+        const openPlantNavBtns = await ui.openPlantNavBtn.findAll();
+        // Open options for the first plant
+        userEvent.click(openPlantNavBtns[0]);
+        userEvent.click(ui.deleteBtn.get());
+        expect(ui.alert.get()).toBeInTheDocument();
       });
 
-      test('confirming plant deletion removes the plant from the list and renders status message or no plants message if the deleted plant was the last one on the list (when the API call is successful)', async () => {
+      test('confirming plant deletion removes the plant from the list and renders status message (when the API call is successful)', async () => {
         mockedAxios.get.mockResolvedValueOnce({
-          data: { plants: [mockPlantsData[0], mockPlantsData[1]] }
+          data: { plants: mockPlantsData }
         });
-        const { rerender } = renderCalendarWithLoading();
+        renderCalendarWithLoading();
+        // Mock data should contain at least two plants, as deleting the last plant should render No Plants view (covered in another test)
+        expect(mockPlantsData.length > 1).toBeTruthy();
         expect(mockedAxios.get).toHaveBeenCalledTimes(1);
-        await screen.findAllByRole('navigation', { name: /plant options/i });
-        const openOptionsButtons = screen.getAllByRole('button', {
-          name: /open options/i
-        });
+        const openPlantNavBtns = await ui.openPlantNavBtn.findAll();
         // Open options for the first plant
-        userEvent.click(openOptionsButtons[0]);
-        userEvent.click(screen.getByRole('button', { name: /delete/i }));
+        userEvent.click(openPlantNavBtns[0]);
+        userEvent.click(ui.deleteBtn.get());
 
-        const confirmationAlert = screen.getByRole('alertdialog', {
-          name: /confirm deletion/i
-        });
-        userEvent.click(
-          within(confirmationAlert).getByRole('button', { name: /delete/i })
-        );
-        mockedAxios.delete.mockResolvedValueOnce({
-          data: { id: mockPlantsData[0]._id }
-        });
-        rerender(
-          <Provider store={store}>
-            <CalendarWithLoading />
-          </Provider>
-        );
+        let confirmationAlert = ui.alert.get();
+        userEvent.click(ui.deleteBtn.get(confirmationAlert));
         expect(mockedAxios.delete).toHaveBeenCalledTimes(1);
-        await screen.findByRole('region', { name: /calendar/i });
+        await ui.calendar.find();
         expect(
           screen.queryByRole('heading', { name: mockPlantsData[0].name })
         ).not.toBeInTheDocument();
-        expect(screen.getByRole('status')).toBeInTheDocument();
-        expect(screen.getAllByRole('listitem').length).toBe(1);
-        userEvent.click(screen.getByRole('button', { name: /open options/i }));
-        userEvent.click(screen.getByRole('button', { name: /delete/i }));
-        const confirmationAlert2 = screen.getByRole('alertdialog', {
-          name: /confirm deletion/i
-        });
-        userEvent.click(
-          within(confirmationAlert2).getByRole('button', { name: /delete/i })
-        );
-        mockedAxios.delete.mockResolvedValueOnce({
-          data: { id: mockPlantsData[0]._id }
-        });
-        rerender(
-          <Provider store={store}>
-            <CalendarWithLoading />
-          </Provider>
-        );
-        expect(mockedAxios.delete).toHaveBeenCalledTimes(2);
-        await screen.findByRole('region', { name: /calendar/i });
-        expect(
-          screen.getByRole('heading', { name: /no plants/i })
-        ).toBeInTheDocument();
+        expect(ui.statusMsg.get()).toBeInTheDocument();
+        expect(ui.statusMsg.get()).toHaveTextContent(/plant deleted/i);
+        expect(ui.listitem.getAll().length).toBe(mockPlantsData.length - 1);
       });
 
       test.todo(
